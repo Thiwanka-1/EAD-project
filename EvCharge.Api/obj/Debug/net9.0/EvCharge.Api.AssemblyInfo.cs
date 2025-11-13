62b16a816d068f0b38522d07788e50b6d23ed36d
Thiwanka-1 <128372987+Thiwanka-1@users.noreply.github.com>
2025-11-13 15:08:14 +0530

    fix issue

--- EvCharge.Api/obj/Debug/net9.0/EvCharge.Api.AssemblyInfo.cs
+++ EvCharge.Api/obj/Debug/net9.0/EvCharge.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EvCharge.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98bf853e91b32ed8cd78d5ed67e13e0fab5a984e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b046b30ebeea7c8de607adba25de4653d43a5cdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("EvCharge.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EvCharge.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
